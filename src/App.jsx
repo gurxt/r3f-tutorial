384d6e32684f2e890adf744b6968d2de86d218c0
gurxt <jung_jungian@outlook.com>
2023-07-17 12:16:08 -0400

    initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Canvas } from '@react-three/fiber'
+import './App.css'
+import Scene from './components/Scene'
+
+const App = () => {
+  return (
+    <Canvas>
+      <Scene />
+    </Canvas>
+  )
+}
+
+export default App
---
+++ src/components/Scene.jsx
@@ -0,0 +1,11 @@
+export default function Scene() {
+  return (
+    <>
+    <mesh>
+      <torusKnotGeometry />
+      <meshNormalMaterial />
+    </mesh>
+    </>
+  )
+}
+
